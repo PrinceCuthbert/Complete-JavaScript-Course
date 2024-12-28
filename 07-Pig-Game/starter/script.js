10915e99c5ced05c79b05eb20bd02b5d09f97ba6
unknown <ishimweprincecuthbert@gmail.com>
2024-12-28 20:45:43 +0200

    Save local changes before merging

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +15,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -27,4 +34,12 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore = currentScore + dice;
+    current0El.textContent = currentScore; // Change later
+  } else {
+    // Switch to next player
+  }
 });
